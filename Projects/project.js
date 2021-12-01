02ff8a5c89dd3b8ea78ff73436a70522c91ec08a
= <=>
2021-12-02 00:37:14 +0530

    Added Filter Feature

--- Projects/project.js
+++ Projects/project.js
@@ -22,3 +22,35 @@ const searchICCProject = () => {
         }
     }
 }
+
+const filterTechStack = (filter) => {
+    // this.classList.add('active');
+    // console.log(this.classList);
+    const projectCard = document.querySelectorAll('.projectCard');
+    projectCard.forEach((item) => {
+        if (filter === 'all') {
+            item.style.display = '';
+        } else {
+            if (item.classList.contains(filter)) {
+                item.style.display = '';
+            } else {
+                item.style.display = 'none';
+            }
+        }
+    })
+
+    const btnContainer = document.getElementById('tech-stack-id');
+    const btns = btnContainer.getElementsByClassName('tech-stack-name');
+    for (let i = 0; i < btns.length; i++) {
+        const element = btns[i];
+        element.addEventListener("click", function () {
+            var current = document.getElementsByClassName("active");
+
+            if (current.length > 0) {
+                current[0].className = current[0].className.replace(" active", "");
+            }
+
+            this.className += " active";
+        });
+    }
+}
